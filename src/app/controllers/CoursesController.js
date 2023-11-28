5c4d42a1129d08c40807406816ddfc4ab372cd8a
Anh Khoa <phunganhkhoa123@gmail.com>
2023-11-28 19:35:37 +0700

    28. [CRUD] Create new course

--- src/app/controllers/CoursesController.js
+++ src/app/controllers/CoursesController.js
@@ -1,20 +1,40 @@
 const Course = require("../models/Course");
-const { multipleMongooseToObject, mongooseToObject } = require("../../util/mongoose");
+const {
+  multipleMongooseToObject,
+  mongooseToObject,
+} = require("../../util/mongoose");
 
 class CoursesController {
-  
-    // [GET] /courses/:slug
-    async show(req, res, next) {
-        try {
-            const course = await Course.findOne({ slug: req.params.slug }).exec();
-            res.render('courses/show', { 
-                course: mongooseToObject(course),
-            });
-        } catch (next) {
-            console.log("Có lỗi");
-        }
+  // [GET] /courses/:slug
+  async show(req, res, next) {
+    try {
+      const course = await Course.findOne({ slug: req.params.slug }).exec();
+      res.render("courses/show", {
+        course: mongooseToObject(course),
+      });
+    } catch (next) {
+      console.log("Có lỗi");
     }
   }
-  
-  module.exports = new CoursesController();
-  
+
+  // [GET] /courses/create
+  create(req, res, next) {
+    res.render("courses/create")
+  }
+
+  // [POST] /courses/store
+  async store(req, res, next) {
+    try {
+        const formData = req.body
+        formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`
+        await Course.create(formData);
+
+        // res.send("Gửi Form thành công")
+        res.redirect('/')
+    } catch (next) {
+        console.log("Error: " + next);
+    }
+  }
+}
+
+module.exports = new CoursesController();
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
+const slug = require('mongoose-slug-updater');
+
+mongoose.plugin(slug);
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const Course = new Schema({
-  name: { type: String },
-  desciption: { type: String },
+  name: { type: String, required: true},
+  description: { type: String },
   image: { type: String },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
+  videoId: { type: String, required: true },
+  slug: { type: String, slug: "name", unique: true },
+}, {
+  timestamps: true,
 });
 
 module.exports = mongoose.model("Course", Course);
